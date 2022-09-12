14634eb02b3de4a9695a4fe9e9cdfc6a8da7f2c5
matheusmafraoandrade <matheusmafrabh@gmail.com>
2022-09-12 19:27:24 -0300

    Impedir duplicatas

--- hacktown.py
+++ hacktown.py
@@ -180,6 +180,7 @@ with tab2:
 
     if adicionar:
         st.session_state.minha_prog = pd.concat([st.session_state.minha_prog, df_selected])
+        st.session_state.minha_prog.drop_duplicates(inplace=True)
 
     if remover:
         try:
